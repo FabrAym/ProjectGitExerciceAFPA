7a4b4fe114c9f7bf02d18e4d794fd593bdea658f
FabAym <faymonino@hotmail.com>
2018-05-31 10:13:13 +0200

    mon commit initial de projet Git

---
+++ src/main/java/ProjectGit/ProjectGit.java
@@ -0,0 +1,11 @@
+package main.java.ProjectGit;
+
+public class ProjectGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Bonjour Bordeaux!!" );
+
+	}
+
+}
